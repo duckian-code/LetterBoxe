bfd45f2a41fafe0e679fcc6bd0b69cf6e6cf1655
Melanie Murray <melaniemurr13@gmail.com>
2026-02-19 10:43:58 -0700

    feat: update schema

--- public/db/schema.ts
+++ public/db/schema.ts
@@ -2,7 +2,32 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    age: integer().notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
+    userName: varchar({ length: 255 }).notNull(),
+    avatarUrl: varchar({ length: 255 }).notNull(),
+    createdAt: varchar({ length: 255 }).notNull(),
 });
+
+export const filmsTable = pgTable("films", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    title: varchar({ length: 255 }).notNull(),
+    year: integer().notNull(),
+    posterUrl: varchar({ length: 255 }).notNull(),
+    createdAt: varchar({ length: 255 }).notNull(),
+});
+
+export const userLikes = pgTable("user_likes", {
+    userId: integer().notNull().unique(),
+    filmId: integer().notNull().unique(),
+    createdAt: varchar({ length: 255 }).notNull(),
+});
+
+export const userWatchlist = pgTable("user_watchlist", {
+    userId: integer().notNull().unique(),
+    filmId: integer().notNull().unique(),
+    createdAt: varchar({ length: 255 }).notNull(),
+});
+
+export const userWatched = pgTable("user_watched", {
+    userId: integer().notNull().unique(),
+    watchDate: varchar({ length: 255 }).notNull(),
+})
